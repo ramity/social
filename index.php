67722a61a6e39aae5a9d2d7730c3ddb0913b976e
ramity <lcb005@uark.edu>
2016-05-31 12:13:46 -0500

    init commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+	<head>
+		<title>Social</title>
+		<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+		<link href="https://ramity.com/projects/social/css/index.css" rel="stylesheet" type="text/css" >
+	</head>
+	<body>
+		<div id="topbar">
+			<div id="topbarinr">
+				<div id="topbarlogo">Ramity</div>
+				<div id="topbarlogin">
+					<a href="https://ramity.com/projects/health/login">login</a>
+					 | 
+					<a href="https://ramity.com/projects/health/login">register</a>
+				</div>
+			</div>
+		</div>
+		<div id="content">
+			<div id="contentinr">
+				<div class="panel">
+					<div class="panelside">
+						<div class="panelheader">Login</div>
+						<form class="panelform">
+							<input type="text" class="panelminimal" placeholder="username or email">
+							<input type="password" class="panelminimal" placeholder="password">
+							<input type="submit" class="panelminimal" value="login">
+						</form>
+					</div>
+					<div class="panelside">
+						<div class="panelheader">Register</div>
+						<form class="panelform">
+							<input type="text" class="panelminimal" placeholder="username">
+							<input type="email" class="panelminimal" placeholder="email">
+							<input type="password" class="panelminimal" placeholder="password">
+							<input type="password" class="panelminimal" placeholder="confirm password">
+							<div id="paneldisclaimer">By creating an account you accept our terms of service.</div>
+							<input type="submit" class="panelminimal" value="create account">
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
